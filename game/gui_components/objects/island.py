e7d26c6a4ee2314f85f9535194feba33703d0e05
Sombreroboy <83668223+Sombreroboy@users.noreply.github.com>
2021-07-09 11:59:36 -0600

    Update island.py

--- game/gui_components/objects/island.py
+++ game/gui_components/objects/island.py
@@ -3,11 +3,20 @@ from game.gui_components.point import Point
 from constants import (
     SCREEN_WIDTH as screen_width,
     SCREEN_HEIGHT as screen_height)
+import random as random
 
 class Island(Game_Object):
     def __init__(self,centered_object, **kwargs):
         super().__init__(**kwargs)
-        self.image = "island"
+        image = random.randint(1,4)
+        if image == 1:
+            self.image = "Island_1"
+        elif image == 2:
+            self.image = "Island_2"
+        elif image == 3:
+            self.image = "Island_3"
+        else image == 4:
+            self.image = "Island_4"
         self.centered_object = centered_object.center
 
     def draw(self):
